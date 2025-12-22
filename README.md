87e5ee0d625fad8fbdc9c30352bfa5646aa9db52
I583880 <simeon.gelovski@sap.com>
2025-12-22 10:50:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# console-banking-app
---
+++ src/main/java/com/mentoring/program/Main.java
@@ -0,0 +1,18 @@
+package com.mentoring.program;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double currentBalance = 0.0;
+        boolean running = true;
+
+        while (running) {
+            // Display menu
+            // Read user choice
+            // Perform actions based on user choice
+        }
+        scanner.close();
+    }
+}
